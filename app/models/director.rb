7e2b5c47ef8ac827cab30bd2cb138fe190fc8eea
R. Sinhue Ruiz Herrera <renesinhue@gmail.com>
2023-04-19 17:29:37 +0000

    MSM queries directors

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,7 +1,19 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :integer          not null, primary key
+#  bio        :text
+#  dob        :date
+#  image      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Director <ApplicationRecord
 
-  def Director.all
+ # def Director.all
     
-  end
+  #end
 
 end
