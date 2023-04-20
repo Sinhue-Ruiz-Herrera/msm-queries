5a3e4763d10c008f8125f4a2f17498f25dc409c8
R. Sinhue Ruiz Herrera <renesinhue@gmail.com>
2023-04-20 21:27:54 +0000

    msm queries almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get("/directors/:an_id", {:controller => "directors", :action => "index" })
+  get("/directors/", {:controller => "directors", :action => "index" })
   get("/directors/eldest", {:controller => "directors", :action => "wisest" })
   get("/directors/youngest", {:controller => "directors", :action => "youngest_director" })
 
+  get("/directors/:an_id", {:controller => "directors", :action => "director_details" })
 
   get("/", { :controller => "application", :action => "homepage" })
 
